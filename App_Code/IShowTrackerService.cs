d6d2d6576f1668fc8803534e15cc62b49947ef90
souhaAmor <samor001@seattlecentral.edu>
2016-06-16 22:22:36 -0700

    create service2

---
+++ App_Code/IShowTrackerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShowTrackerService" in both code and config file together.
+[ServiceContract]
+public interface IShowTrackerService
+{
+    [OperationContract]
+    List<string> GetVenues();
+
+    [OperationContract]
+    List<string> GetArtists();
+
+    [OperationContract]
+    List<string> GetShows();
+
+    [OperationContract]
+    List<ShowInfo> GetShowByVenue(string venue);
+
+    [OperationContract]
+    List<ShowInfo> GetShowByArtist(string artist);
+}//end interface
+
+[DataContract]
+public class ShowInfo
+{
+    [DataMember]
+    public string ShowName { set; get; }
+
+    [DataMember]
+    public string ShowDate { set; get; }//uppercase because it's instance
+
+    [DataMember]
+    public string ShowTime { set; get; }
+
+    [DataMember]
+    public string VenueName { set; get; }
+
+}//end class
---
+++ App_Code/ShowTrackerService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShowTrackerService" in code, svc and config file together.
+public class ShowTrackerService : IShowTrackerService
+{
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    public List<string> GetArtists()
+    {
+        var artistquery = from a in db.Artists
+                    orderby a.ArtistName
+                    select new { a.ArtistName };
+        List<string> artistList = new List<string>();
+        foreach (var a in artistquery)
+        {
+            artistList.Add(a.ArtistName.ToString());
+        }
+        return artistList;
+    }
+
+    public List<ShowInfo> GetShowByArtist(string artist)
+    {
+        var showByArtistQuery = from s in db.ShowDetails
+                               where s.Artist.ArtistName.Equals(artist)
+                               orderby s.Show.ShowName
+                               select new
+                               {
+                                   s.Show.ShowName,
+                                   s.Show.ShowDate,
+                                   s.Show.ShowTime,
+                                   s.Show.Venue.VenueName
+                               };
+        List<ShowInfo> info = new List<ShowInfo>();
+
+        foreach (var s in showByArtistQuery)
+        {
+            ShowInfo si = new ShowInfo();
+            si.ShowName = s.ShowName;
+            si.ShowDate = s.ShowDate.ToString();
+            si.ShowTime = s.ShowTime.ToString();
+            si.VenueName = s.VenueName;
+
+            info.Add(si);
+        }
+        return info;
+    }
+
+    public List<ShowInfo> GetShowByVenue(string venue)
+    {
+        var showByVenueQuery = from s in db.Shows
+                               where s.Venue.VenueName.Equals(venue)
+                               orderby s.ShowName
+                               select new {
+                                   s.ShowName,
+                                   s.ShowDate,
+                                   s.ShowTime
+                               };
+        List<ShowInfo> info = new List<ShowInfo>();
+
+        foreach (var s in showByVenueQuery)
+        {
+            ShowInfo si = new ShowInfo();
+            si.ShowName = s.ShowName;
+            si.ShowDate = s.ShowDate.ToString();
+            si.ShowTime = s.ShowTime.ToString();
+           
+            info.Add(si);
+        }
+        return info;
+    }
+
+    public List<string> GetShows()
+    {
+        var showquery = from s in db.Shows
+                          orderby s.ShowName
+                          select new { s.ShowName };
+        List<string> showList = new List<string>();
+        foreach (var s in showquery)
+        {
+            showList.Add(s.ShowName.ToString());
+        }
+        return showList;
+    }
+
+    public List<string> GetVenues()
+    {
+        var venuequery = from v in db.Venues
+                        orderby v.VenueName
+                        select new { v.VenueName };
+        List<string> venueList = new List<string>();
+        foreach (var v in venuequery)
+        {
+            venueList.Add(v.VenueName.ToString());
+        }
+        return venueList;
+    }
+}
